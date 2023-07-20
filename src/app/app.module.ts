c972ca73b1f457034caeef402a13505de86f8a99
navin <navinvannam1999@gmail.com>
2023-07-20 10:21:03 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SigninComponent } from './homepage/signin/signin.component';
+import { SignupComponent } from './homepage/signup/signup.component';
+import { AboutUsComponent } from './homepage/about-us/about-us.component';
+import { ContactusComponent } from './homepage/contactus/contactus.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    SigninComponent,
+    SignupComponent,
+    AboutUsComponent,
+    ContactusComponent
+
+    
   ],
   imports: [
     BrowserModule,
